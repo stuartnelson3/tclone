11509f214bec9f56d279235ff8d4e77bfe8721f2
Stuart Nelson <stuartnelson3@gmail.com>
2013-05-27 23:49:59 -0500

    user validation tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,32 @@
 require 'spec_helper'
 
 describe User do
-  let(:user) { User.new }
+  let(:user) { User.new(user_name: "sample_user", email: "example@example.com", password: "example_password") }
+
+  context "validations" do
+    it "shouldn't allow a user_name with fewer than 3 characters" do
+      user.user_name = "aa"
+      expect(user.valid?).to eq(false)
+    end
+    it "shouldn't allow a user_name with more 20 characters" do
+      user.user_name = "a" * 21
+      expect(user.valid?).to eq(false)
+    end
+    it "shouldn't allow a user's info to exceed 140 characters" do
+      user.info = "a" * 141
+      expect(user.valid?).to eq(false)
+    end
+  end
 
   context "metaprogramming methods" do
     it "should respond to #tweets_count" do
-      user.respond_to?(:tweets_count).should be_true
+      expect(user.respond_to?(:tweets_count)).to eq(true)
     end
     it "should respond to #following_count" do
-      user.respond_to?(:following_count).should be_true
+      expect(user.respond_to?(:following_count)).to eq(true)
     end
     it "should respond to #followers_count" do
-      user.respond_to?(:followers_count).should be_true
+      expect(user.respond_to?(:followers_count)).to eq(true)
     end
   end
 
