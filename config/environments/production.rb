0d4ae38c475749c928e8f6d351cf099971bb1345
Stuart Nelson <stuartnelson3@gmail.com>
2013-05-28 13:26:24 -0500

    heroku domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,7 @@ Tclone::Application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'pure-eyrie-6351.herokuapp.com',
     :authentication => :plain,
   }
   config.action_mailer.delivery_method = :smtp
