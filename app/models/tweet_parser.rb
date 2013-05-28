b8351f52f9184da010d67cb473fee48d6d876b4e
Stuart Nelson <stuartnelson3@gmail.com>
2013-05-28 12:43:24 -0500

    changed tests, memoize repeated call

--- app/models/tweet_parser.rb
+++ app/models/tweet_parser.rb
@@ -6,20 +6,28 @@ class TweetParser
   end
 
   def parse
-    parse_user_names.each do |user_name|
-      text.gsub!(user_name, make_link(user_name))
-    end
-    parse_user_names.each do |user_name|
+    substitute_mentions_for_links
+    send_mention_emails
+    text
+  end
+
+  def send_mention_emails
+    parsed_user_names.each do |user_name|
       MentionMailer.mention_email(tweet, user_name[1..-1]).deliver
     end
-    text
+  end
+
+  def substitute_mentions_for_links
+    parsed_user_names.each do |user_name|
+      text.gsub!(user_name, make_link(user_name))
+    end
   end
 
   def make_link(user_name)
     "<a href='/#{user_name[1..-1]}' class='mention-link'>#{user_name}</a>"
   end
 
-  def parse_user_names
-    text.scan(/@{1}[a-zA-Z1-9_]+/i).flatten
+  def parsed_user_names
+    @parsed_user_names ||= text.scan(/@{1}[a-zA-Z1-9_]+/i).flatten
   end
 end
--- spec/models/tweet_parser_spec.rb
+++ spec/models/tweet_parser_spec.rb
@@ -1,20 +1,21 @@
 require 'spec_helper'
 
 describe TweetParser do
-  let(:tweet_parser) { TweetParser.new("Mention @user and @another_user and @user4") }
+  let(:tweet) { Tweet.new(text: "Mention @user and @another_user") }
+  let(:tweet_parser) { TweetParser.new(tweet) }
 
   context "parsing a tweet" do
     it "should pull out user_names" do
-      expect(tweet_parser.parse_user_names).to eq(["@user", "@another_user", "@user4"])
+      expect(tweet_parser.parsed_user_names).to eq(["@user", "@another_user"])
     end
 
     it "should turn the username into a link" do
-      anchor_text = "<a href='/user' class='mention-link'>@user</a>"
-      expect(tweet_parser.make_link("@user")).to eq(anchor_text)
+      anchor_text = "<a href='/user4' class='mention-link'>@user4</a>"
+      expect(tweet_parser.make_link("@user4")).to eq(anchor_text)
     end
 
     it "should return the text with links to the usernames" do
-      parsed_tweet = "Mention <a href='/user' class='mention-link'>@user</a> and <a href='/another_user' class='mention-link'>@another_user</a> and <a href='/user4' class='mention-link'>@user4</a>"
+      parsed_tweet = "Mention <a href='/user' class='mention-link'>@user</a> and <a href='/another_user' class='mention-link'>@another_user</a>"
       expect(tweet_parser.parse).to eq(parsed_tweet)
     end
   end
