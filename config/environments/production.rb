7c1b1aba825a9bfa60a9b3547a6e64636333b1e4
Stuart Nelson <stuartnelson3@gmail.com>
2013-05-28 13:23:12 -0500

    mail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,14 @@
 Tclone::Application.configure do
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'yourapp.heroku.com',
+    :authentication => :plain,
+  }
+  config.action_mailer.delivery_method = :smtp
   # config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
-  # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   :address => "127.0.0.1",
   #   :port    => 25,
